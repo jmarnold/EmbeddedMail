368a495205b219c83967cb851c239c49d821e502
jmarnold <rncodepoet@gmail.com>
2012-03-25 16:08:21 -0500

    Removed FubuCore dependency

---
+++ src/EmbeddedMail/CoreExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmbeddedMail
+{
+    internal static class CoreExtensions
+    {
+         public static void Each<T>(this IEnumerable<T> source, Action<T> callback)
+         {
+             foreach(var value in source)
+             {
+                 callback(value);
+             }
+         }
+    }
+}
