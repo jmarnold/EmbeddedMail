790fbdd27ede020761108e97dce2a574280fe5fa
Dennis Meenhuis <mail@dennus.net>
2012-04-03 12:33:00 +0200

    Included a test for the previously commited bug fix

--- src/EmbeddedMail.Tests/EmbeddedSmtpServerIntegratedTester.cs
+++ src/EmbeddedMail.Tests/EmbeddedSmtpServerIntegratedTester.cs
@@ -10,6 +10,8 @@ namespace EmbeddedMail.Tests
     {
         private EmbeddedSmtpServer theServer;
         private MailMessage theMessage;
+        private const string ccAddress = "copy@domain.com";
+        private const string bccAddress = "blind@domain.com";
 
         [TestFixtureSetUp]
         public void BeforeAll()
@@ -17,12 +19,15 @@ namespace EmbeddedMail.Tests
             var port = PortFinder.FindPort(8181);
             theServer = new EmbeddedSmtpServer(port);
             theMessage = new MailMessage("x@domain.com", "y@domain.com", "Hello there", "O hai");
-            //theMessage.CC.Add("copy@domain.com");
-            theMessage.Bcc.Add("blind@domain.com");
+            theMessage.CC.Add(ccAddress);
+            theMessage.Bcc.Add(bccAddress);
             theServer.Start();
 
             using (var client = new SmtpClient("localhost", port))
             {
+                client.Send(theMessage);
+
+                theMessage.CC.Clear();
                 client.Send(theMessage);
 
                 theMessage.Attachments.Add(new Attachment("Attachment1.txt"));
@@ -42,14 +47,22 @@ namespace EmbeddedMail.Tests
             message.Subject.ShouldEqual(theMessage.Subject);
             message.Body.ShouldEqual(theMessage.Body);
 
-            //message.CC.Single().ShouldEqual(theMessage.CC.Single());
-            message.Bcc.Single().ShouldEqual(theMessage.Bcc.Single());
+            message.CC.Single().Address.ShouldEqual(ccAddress);
+            message.Bcc.Single().Address.ShouldEqual(bccAddress);
         }
 
         [Test]
         public void receives_multiple_messages()
         {
-            theServer.ReceivedMessages().ShouldHaveCount(2);
+            theServer.ReceivedMessages().ShouldHaveCount(3);
+        }
+
+        [Test]
+        public void receives_message_without_CC_header()
+        {
+            var message = theServer.ReceivedMessages().Skip(1).First();
+
+            message.CC.Count.ShouldEqual(0);
         }
 
         [Test]
