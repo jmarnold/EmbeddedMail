4e85e19883618762f0942904735eb329d9a4e687
Kyle Blocher <kyle.blocher@blocherconsulting.com>
2016-01-27 12:16:41 -0600

    Exposed session (and message) listener

--- src/EmbeddedMail/EmbeddedSmtpServer.cs
+++ src/EmbeddedMail/EmbeddedSmtpServer.cs
@@ -7,157 +7,135 @@ using System.Net.Sockets;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace EmbeddedMail
-{
-    public interface ISmtpServer : IDisposable
-    {
-        IEnumerable<MailMessage> ReceivedMessages();
-        void Start();
-        void Stop();
+namespace EmbeddedMail {
+  public interface ISmtpServer : IDisposable {
+    IEnumerable<MailMessage> ReceivedMessages();
+    void Start();
+    void Stop();
+  }
+
+  public class EmbeddedSmtpServer : ISmtpServer {
+    private readonly IList<MailMessage> _messages = new List<MailMessage>();
+    private readonly IList<ISmtpSession> _sessions = new List<ISmtpSession>();
+    private bool _closed;
+
+    public EmbeddedSmtpServer(int port = 25)
+        : this(IPAddress.Any, port) {
     }
 
-    public class EmbeddedSmtpServer : ISmtpServer
-    {
-        private readonly IList<MailMessage> _messages = new List<MailMessage>();
-        private readonly IList<ISmtpSession> _sessions = new List<ISmtpSession>(); 
-        private bool _closed;
-
-        public EmbeddedSmtpServer(int port = 25)
-            : this(IPAddress.Any, port)
-        {
-        }
-
-        public EmbeddedSmtpServer(IPAddress address, int port = 25)
-        {
-            Address = address;
-            Port = port;
-
-            Listener = new TcpListener(Address, port);
-        }
-
-        public TcpListener Listener { get; private set; }
-        public IPAddress Address { get; private set; }
-        public int Port { get; private set; }
-
-        public IEnumerable<MailMessage> ReceivedMessages()
-        {
-            return _messages;
-        }
-
-        public void Start()
-        {
-            Listener.Start();
-            SmtpLog.Info(string.Format("Server started at {0}", new IPEndPoint(Address, Port)));
-            ListenForClients();
-        }
-
-        public void WaitForMessages(int timeoutInMilliseconds = 5000)
-        {
-            var count = _messages.Count;
-            Wait.Until(() => _messages.Count > count, timeoutInMilliseconds: timeoutInMilliseconds);
-        }
-
-        public void Stop()
-        {
-            _closed = true;
-        }
-
-        public void ListenForClients()
-        {
-            if (_closed) return;
-            ListenForClients(OnClientConnect, handleException);
-        }
-
-        private void handleException(Exception exc)
-        {
-            if (exc is AggregateException)
-            {
-                var inner = exc.InnerException;
-                if(inner != null) handleException(exc);
-                return;
-            }
-
-            if (exc is ObjectDisposedException) return;
-
-            SmtpLog.Error("Listener socket is closed", exc);
-        }
-
-        public Task ListenForClients(Action<ISocket> callback, Action<Exception> error)
-        {
-            try
-            {
-                Func<IAsyncResult, ISocket> end = r => new SocketWrapper(Listener.EndAcceptSocket(r));
-
-                var task = Task.Factory.FromAsync(Listener.BeginAcceptSocket, end, null);
-
-                task.ContinueWith(t => callback(t.Result), TaskContinuationOptions.NotOnFaulted)
-                    .ContinueWith(t => error(t.Exception), TaskContinuationOptions.OnlyOnFaulted);
-
-                return task;
-
-            }
-            catch(Exception e)
-            {
-                error(e);
-                return Task.Factory.StartNew(() => { });
-            }
-            
-        }
-
-        public void OnClientConnect(ISocket clientSocket)
-        {
-            if (_closed) return;
-
-            SmtpLog.Info("Client connected");
-            ListenForClients();
-
-            var session = new SmtpSession(clientSocket)
-            {
-                OnMessage = (msg) => _messages.Add(msg)
-            };
-            session.Start();
-
-            _sessions.Add(session);
-        }
-
-        public void Dispose()
-        {
-            Stop();
-            // I don't grok the disposal lifecycle for the sockets yet
-            Listener.Stop();
-
-            _sessions.Each(x => x.Dispose());
-        }
-
-        /// <summary>
-        /// Creates a new instance fot the <see cref="EmbeddedSmtpServer"/> class
-        /// by finding the first open port starting at the specified port.
-        /// </summary>
-        /// <param name="startingPort">The port to start scanning from (default 25)</param>
-        /// <returns></returns>
-        public static EmbeddedSmtpServer Local(int startingPort = 8080)
-        {
-            var port = PortFinder.FindPort(startingPort);
-            return new EmbeddedSmtpServer(port);
-        }
+    public EmbeddedSmtpServer(IPAddress address, int port = 25) {
+      Address = address;
+      Port = port;
+      Listener = new TcpListener(Address, port);
     }
 
-    public static class Wait
-    {
-        public static void Until(Func<bool> condition, int millisecondPolling = 500, int timeoutInMilliseconds = 5000)
-        {
-            if (condition()) return;
+    public TcpListener Listener { get; private set; }
+    public IPAddress Address { get; private set; }
+    public int Port { get; private set; }
 
-            var clock = new Stopwatch();
-            clock.Start();
-
-            while (clock.ElapsedMilliseconds < timeoutInMilliseconds)
-            {
-                Thread.Yield();
-                Thread.Sleep(500);
-
-                if (condition()) return;
-            }
-        }
+    public IEnumerable<MailMessage> ReceivedMessages() {
+      return _messages;
     }
+
+    public Action<SmtpSession> OnSessionStart { get; set; }
+
+    public void Start() {
+      Listener.Start();
+      SmtpLog.Info(string.Format("Server started at {0}", new IPEndPoint(Address, Port)));
+      ListenForClients();
+    }
+
+    public void WaitForMessages(int timeoutInMilliseconds = 5000) {
+      var count = _messages.Count;
+      Wait.Until(() => _messages.Count > count, timeoutInMilliseconds: timeoutInMilliseconds);
+    }
+
+    public void Stop() {
+      _closed = true;
+    }
+
+    private void ListenForClients() {
+      if (_closed) return;
+      this.ListenForClients(OnClientConnect, OnClientException);
+    }
+
+    private void OnClientException(Exception exc) {
+      if (exc is AggregateException) {
+        var inner = exc.InnerException;
+        if (inner != null) OnClientException(inner);
+        return;
+      }
+
+      if (exc is ObjectDisposedException) return;
+
+      SmtpLog.Error("Listener socket is closed", exc);
+    }
+
+    private Task ListenForClients(Action<ISocket> callback, Action<Exception> error) {
+      try {
+        Func<IAsyncResult, ISocket> end = r => new SocketWrapper(Listener.EndAcceptSocket(r));
+
+        var task = Task.Factory.FromAsync(Listener.BeginAcceptSocket, end, null);
+
+        task.ContinueWith(t => callback(t.Result), TaskContinuationOptions.NotOnFaulted)
+            .ContinueWith(t => error(t.Exception), TaskContinuationOptions.OnlyOnFaulted);
+
+        return task;
+
+      } catch (Exception e) {
+        error(e);
+        return Task.Factory.StartNew(() => { });
+      }
+
+    }
+
+    public void OnClientConnect(ISocket clientSocket) {
+      if (_closed) return;
+
+      SmtpLog.Info("Client connected");
+      ListenForClients();
+
+      var session = new SmtpSession(clientSocket);
+      session.OnMessage.Add(_messages.Add);
+      if (this.OnSessionStart != null) this.OnSessionStart(session);
+      session.Start();
+
+      _sessions.Add(session);
+    }
+
+    public void Dispose() {
+      Stop();
+      // I don't grok the disposal lifecycle for the sockets yet
+      Listener.Stop();
+
+      _sessions.Each(x => x.Dispose());
+    }
+
+    /// <summary>
+    /// Creates a new instance fot the <see cref="EmbeddedSmtpServer"/> class
+    /// by finding the first open port starting at the specified port.
+    /// </summary>
+    /// <param name="startingPort">The port to start scanning from (default 25)</param>
+    /// <returns></returns>
+    public static EmbeddedSmtpServer Local(int startingPort = 8080) {
+      var port = PortFinder.FindPort(startingPort);
+      return new EmbeddedSmtpServer(port);
+    }
+  }
+
+  public static class Wait {
+    public static void Until(Func<bool> condition, int millisecondPolling = 500, int timeoutInMilliseconds = 5000) {
+      if (condition()) return;
+
+      var clock = new Stopwatch();
+      clock.Start();
+
+      while (clock.ElapsedMilliseconds < timeoutInMilliseconds) {
+        Thread.Yield();
+        Thread.Sleep(500);
+
+        if (condition()) return;
+      }
+    }
+  }
 }
--- src/EmbeddedMail/ISmtpSession.cs
+++ src/EmbeddedMail/ISmtpSession.cs
@@ -5,91 +5,86 @@ using System.Linq;
 using System.Net.Mail;
 using EmbeddedMail.Handlers;
 
-namespace EmbeddedMail
-{
-    public interface ISmtpSession : IDisposable
-    {
-        IEnumerable<string> Recipients { get; }
-        string RemoteAddress { get; }
-        void WriteResponse(string data);
-        void SaveMessage(MailMessage message);
-        void AddRecipient(string address);
+namespace EmbeddedMail {
+  public interface ISmtpSession : IDisposable {
+    IEnumerable<string> Recipients { get; }
+    string RemoteAddress { get; }
+    void WriteResponse(string data);
+    void SaveMessage(MailMessage message);
+    void AddRecipient(string address);
+  }
+
+  public class SmtpSession : ISmtpSession {
+    private readonly ISocket _socket;
+    private StreamWriter _writer;
+    private StreamReader _reader;
+    private readonly IList<MailMessage> _messages = new List<MailMessage>();
+    private readonly IList<string> _recipients = new List<string>();
+
+    public SmtpSession(ISocket socket) {
+      _socket = socket;
+      OnMessage = new List<Action<MailMessage>>();
     }
 
-    public class SmtpSession : ISmtpSession
-    {
-        private readonly ISocket _socket;
-        private StreamWriter _writer;
-        private StreamReader _reader;
-        private readonly IList<MailMessage> _messages = new List<MailMessage>();
-        private readonly IList<string> _recipients = new List<string>(); 
+    public List<Action<MailMessage>> OnMessage { get; private set; }
 
-        public SmtpSession(ISocket socket)
-        {
-            _socket = socket;
-            OnMessage = (msg) => { };
+    public void Start() {
+      if (!_socket.Connected) return;
+
+      _reader = new StreamReader(_socket.Stream);
+      _writer = new StreamWriter(_socket.Stream) { AutoFlush = true };
+
+      _writer.WriteLine("220 localhost Server Ready");
+      var isMessageBody = false;
+      while (_socket.Connected) {
+        SmtpToken token;
+        try {
+          token = SmtpToken.FromLine(_reader.ReadLine(), isMessageBody);
+        } catch (IOException) {
+          break;
         }
 
-        public Action<MailMessage> OnMessage { get; set; }
-
-        public void Start()
-        {
-            if (!_socket.Connected) return;
-
-            _reader = new StreamReader(_socket.Stream);
-            _writer = new StreamWriter(_socket.Stream) { AutoFlush = true };
-            
-            _writer.WriteLine("220 localhost Server Ready");
-            var isMessageBody = false;
-            while(_socket.Connected)
-            {
-                var token = SmtpToken.FromLine(_reader.ReadLine(), isMessageBody);
-                
-                SmtpLog.Debug(token.Data);
-
-                var handler = ProtocolHandlers.HandlerFor(token);
-                if(handler.Handle(token, this) == ContinueProcessing.Stop)
-                {
-                    break;
-                }
-
-                isMessageBody = token.IsData && token.IsMessageBody;
-            }
+        SmtpLog.Debug(token.Data);
+        var handler = ProtocolHandlers.HandlerFor(token);
+        if (handler.Handle(token, this) == ContinueProcessing.Stop) {
+          break;
         }
+        isMessageBody = token.IsData && token.IsMessageBody;
+      }
 
-        public void Dispose()
-        {
-            WriteResponse("421 localhost Closing transmission channel");
-
-            _writer.Close();
-            _reader.Close();
-        }
-
-        public IEnumerable<string> Recipients
-        {
-            get { return _recipients.ToArray(); }
-        }
-
-        public string RemoteAddress
-        {
-            get { return _socket.RemoteIpAddress; }
-        }
-
-        public void WriteResponse(string data)
-        {
-            SmtpLog.Debug(data);
-            _writer.WriteLine(data);
-        }
-
-        public void SaveMessage(MailMessage message)
-        {
-            _messages.Add(message);
-            OnMessage(message);
-        }
-
-        public void AddRecipient(string address)
-        {
-            _recipients.Add(address);
-        }
+      try {
+        _socket.Close();
+        _socket.Dispose();
+      } catch (Exception) { }
     }
+
+    public void Dispose() {
+      WriteResponse("421 localhost Closing transmission channel");
+
+      _writer.Close();
+      _reader.Close();
+    }
+
+    public IEnumerable<string> Recipients {
+      get { return _recipients.ToArray(); }
+    }
+
+    public string RemoteAddress {
+      get { return _socket.RemoteIpAddress; }
+    }
+
+    public void WriteResponse(string data) {
+      SmtpLog.Debug(data);
+      _writer.WriteLine(data);
+    }
+
+    public void SaveMessage(MailMessage message) {
+      _messages.Add(message);
+      OnMessage.ForEach(f => f(message));
+    }
+
+    public void AddRecipient(string address) {
+      _recipients.Add(address);
+    }
+  }
 }
