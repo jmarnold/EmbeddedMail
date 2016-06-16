07c6a01ff20ec94a1ce9f4baa3d15ea26c78274f
Yevgen Reztsov <yevgen.reztsov@blocherconsulting.com>
2016-06-16 12:53:56 -0700

    changes for kyles

--- src/EmbeddedMail/Handlers/AuthPlainHandler.cs
+++ src/EmbeddedMail/Handlers/AuthPlainHandler.cs
@@ -11,9 +11,15 @@ namespace EmbeddedMail.Handlers {
         session.WriteResponse("334");
         return ContinueProcessing.ContinueAuth;
       } else {
+        //This is where actual authentication should happen instead of auto-returning 235 success
+        //For more SMTP protocol: http://www.samlogic.net/articles/smtp-commands-reference-auth.htm
         session.WriteResponse("235 OK");
         return ContinueProcessing.Continue;
       }
+    }
+
+    public virtual bool SmtpAuthorization(string username, string password) {
+      return true;
     }
   }
 }
